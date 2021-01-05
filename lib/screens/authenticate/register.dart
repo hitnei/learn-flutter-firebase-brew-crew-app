cb34b928baf31cf48e42c12db88a6483c01c54e4
hitnei <hoangnhi050298@gmail.com>
2021-01-05 17:04:20 +0700

    toggle between signin signup

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,17 +1,17 @@
 import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
-class SignIn extends StatefulWidget {
+class Register extends StatefulWidget {
 
   final Function toggleView;
-  SignIn({ this.toggleView });
+  Register({ this.toggleView });
 
   @override
-  _SignInState createState() => _SignInState();
+  _RegisterState createState() => _RegisterState();
 }
 
-class _SignInState extends State<SignIn> {
-
+class _RegisterState extends State<Register> {
+  
   final AuthService _auth = AuthService();
 
   // text field state
@@ -25,14 +25,14 @@ class _SignInState extends State<SignIn> {
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
         elevation: 0.0,
-        title: Text('Sign in to Brew Crew'),
+        title: Text('Sign up to Brew Crew'),
         actions: <Widget>[
           FlatButton.icon(
             onPressed: () {
               widget.toggleView();
             }, 
             icon: Icon(Icons.person), 
-            label: Text('Register'),
+            label: Text('Sign in'),
           )
         ]
       ),
@@ -58,7 +58,7 @@ class _SignInState extends State<SignIn> {
               RaisedButton(
                 color: Colors.pink[400],
                 child: Text(
-                  'Sign in',
+                  'Register',
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
