9637e57ae3c334aee5b15fc3ae69a05092e90ef0
hitnei <hoangnhi050298@gmail.com>
2021-01-07 17:24:17 +0700

    slider

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -50,6 +50,21 @@ class _SettingsFormState extends State<SettingsForm> {
               _currentSugars = val;
             });},
           ),
+          SizedBox(height: 20),
+          Slider(
+            value: (_currentStrength ?? 100).toDouble(),
+            activeColor: Colors.brown[_currentStrength ?? 100],
+            inactiveColor: Colors.brown[_currentStrength ?? 100],
+            min: 100.0,
+            max: 900.0,
+            divisions: 8,
+            onChanged: (val) {
+              setState(() {
+                _currentStrength = val.toInt().round();
+              });
+            }
+          ),
+          SizedBox(height: 20),
           RaisedButton(
             color: Colors.pink[400],
             child: Text(
