76fd2a4ca241f2828fe524ac32346c4dda6b1322
hitnei <hoangnhi050298@gmail.com>
2021-01-07 16:15:47 +0700

    brew model

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,5 +1,5 @@
+import 'package:brew_crew/models/brew.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 
 class BrewList extends StatefulWidget {
@@ -11,10 +11,12 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
 
-    final brews = Provider.of<QuerySnapshot>(context);
-    for (var doc in brews.docs) {
-      print(doc.data());
-    }
+    final brews = Provider.of<List<Brew>>(context);
+    brews.forEach((brew) {
+      print(brew.name);
+      print(brew.sugars);
+      print(brew.strength);
+    });
 
     return Container(
       
